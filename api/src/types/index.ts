aa4bbda3bd6657cf660e589d9df856c7b0fb3829
hktips <hemant210433@gmail.com>
2025-02-09 16:00:16 +0530

    Create RedishManager

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -6,3 +6,41 @@ export const GET_OPEN_ORDERS = "GET_OPEN_ORDERS";
 export const GET_DEPTH = "GET_DEPTH";
 
 
+export type MessageFromOrderbook={
+    type:"DEPTH",
+    payload:{
+        market: string,
+        bids:[string,string][],
+        asks:[string,string][],    
+    }
+   
+}|{
+    type:"ORDER_PLACED",
+    payload:{
+        orderId: string,
+        executeQty: number,
+        fills:[
+            {
+                price:string,
+                qty: number
+            }
+        ]
+    }
+}|{
+    type: "ORDER_CANCELLED",
+    payload:{
+        orderId: string,
+        executedQty: number,
+        remainingQty: number
+    }
+}|{
+    type: "OPEN_ORDERS",
+    payload:{
+        orderId:string,
+        executedQty:number,
+        price: string,
+        quantity: string,
+        side:"buy"|"sell",
+        userId:string
+    }[]
+}
