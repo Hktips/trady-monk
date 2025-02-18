6c873e8c8c3cdd63385abafa6ba4c4b19781e1c7
hktips <hemant210433@gmail.com>
2025-02-18 11:01:12 +0530

    Created AddOrder

--- engine/src/trade/Orderbook.ts
+++ engine/src/trade/Orderbook.ts
@@ -14,3 +14,68 @@ export interface Fill {
     otherUserId: string;
     markerOrderId: string;
 }
+
+export class Orderbook {
+    bids: Order[];
+    asks: Order[];
+    baseAsset: string;
+    quoteAsset: string = BASE_CURRENCY;
+    lastTradeId: number;
+    currentPrice: number;
+
+    constructor(baseAsset: string, bids: Order[], asks: Order[], lastTradeId: number, currentPrice: number) {
+        this.bids = bids;
+        this.asks = asks;
+        this.baseAsset = baseAsset;
+        this.lastTradeId = lastTradeId || 0;
+        this.currentPrice = currentPrice ||0;
+    }
+
+    ticker() {
+        return `${this.baseAsset}_${this.quoteAsset}`;
+    }
+
+    getSnapshot() {
+        return {
+            baseAsset: this.baseAsset,
+            bids: this.bids,
+            asks: this.asks,
+            lastTradeId: this.lastTradeId,
+            currentPrice: this.currentPrice
+        }
+    }
+    addOrder(order: Order): {
+        executedQty: number,
+        fills: Fill[]
+    } {
+        if (order.side === "buy") {
+            const {executedQty, fills} = this.matchBid(order); 
+            order.filled = executedQty;
+            if (executedQty === order.quantity) {
+                return {
+                    executedQty,
+                    fills
+                }
+            }
+            this.bids.push(order);
+            return {
+                executedQty,
+                fills
+            }
+        } else {
+            const {executedQty, fills} = this.matchAsk(order);
+            order.filled = executedQty;
+            if (executedQty === order.quantity) {
+                return {
+                    executedQty,
+                    fills
+                }
+            }
+            this.asks.push(order);
+            return {
+                executedQty,
+                fills
+            }
+        }
+    }
+  }
